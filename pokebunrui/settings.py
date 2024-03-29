65d9911ab46d6a68a9c148be24647ed65b51cdeb
pymoto <tt0801160@gmail.com>
2024-03-29 13:23:20 +0000

    host制限

--- pokebunrui/settings.py
+++ pokebunrui/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b9ltkmf07684yx_s28#zvzto=udty8)gu#3&=(9xjsa+^9oe=l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['warabiii.com']
 
 
 # Application definition
